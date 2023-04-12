366a6e63aa841ff75955a854c6cb13f10185140d
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-04-12 17:01:16 +0300

    feat: add store to user module

---
+++ src/app/store/users/auth-api.ts
@@ -0,0 +1,46 @@
+import { createApi } from '@reduxjs/toolkit/dist/query/react';
+import { BASE_QUERY } from '@store/models';
+
+export const authApi = createApi({
+  baseQuery: BASE_QUERY,
+  endpoints: builder => ({
+    createUser: builder.mutation({
+      query(data) {
+        return { url: 'registration', method: 'POST', body: data };
+      },
+    }),
+    loginUser: builder.mutation({
+      query(data) {
+        return {
+          url: 'auth/login',
+          method: 'POST',
+          body: data,
+        };
+      },
+    }),
+    getCurrentUser: builder.query({
+      query: () => {
+        return {
+          url: 'auth/profile',
+          method: 'GET',
+        };
+      },
+    }),
+    updateUser: builder.mutation({
+      query(data) {
+        return {
+          url: `auth/profile`,
+          method: 'PUT',
+          body: data,
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useCreateUserMutation,
+  useLoginUserMutation,
+  useGetCurrentUserQuery,
+  useUpdateUserMutation,
+} = authApi;
---
+++ src/app/store/users/models/user.interface.ts
@@ -0,0 +1,19 @@
+export interface IUserRequest {
+  // id: string;
+  // userName: string;
+  // email: string;
+  userName: string;
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  avatar: any;
+  cardNumber: string[];
+  password: string;
+}
+
+export interface IUserResponse {
+  user: IUserRequest | null;
+  token: string;
+}
